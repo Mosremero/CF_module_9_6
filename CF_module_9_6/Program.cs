4f74f76242a3d1a8b936d1df405fcba07f77cba5
Zakharov Ivan <zakharov_ii@proitr.ru>
2021-08-21 22:27:48 +0300

    Add project files.

---
+++ CF_module_9_6/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CF_module_9_6_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Exception> listOfException = new();
+            listOfException.Add(new MyException("Собственное исключение"));
+            listOfException.Add(new FormatException());
+            listOfException.Add(new IndexOutOfRangeException());
+            listOfException.Add(new DivideByZeroException());
+            listOfException.Add(new ArgumentOutOfRangeException());
+
+            foreach (Exception exc in listOfException)
+            {
+                try
+                {
+                    throw exc;
+                }
+                catch
+                {
+                    Console.WriteLine(exc);
+                }
+            }
+        }
+    }
+
+    public class MyException : Exception
+    {
+        public MyException()
+        {}
+        public MyException(string message) : base(message)
+        {}
+    }
+
+}
---
+++ CF_module_9_6_2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CF_module_9_6_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
